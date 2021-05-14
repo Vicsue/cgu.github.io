3de7c2457ee8469f9fa97043a847abdcba8ea038
Vicsue <37897545+Vicsue@users.noreply.github.com>
2021-05-14 16:26:35 +0000

    Deploying to gh-pages from @ Vicsue/cgu.github.io@8ed121192a5b6c81ad8666467688cdfd2157d222 🚀

---
+++ _next/static/kAQNh1li5hiykHJFVdOj7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fd1e03d2decf710b546d.js"],"/_error":["static/chunks/pages/_error-5c0deeabbe80c8bf7a72.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
