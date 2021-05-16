2de11b640e47a315638697138534ad2195ab1a90
vic <vic@cacdi.com>
2021-05-16 13:04:18 +0800

    dfdfd

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import Head from "next/head";
-
+const prefix = "/cgu.github.io";
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={prefix + "/favicon.ico"} />
       </Head>
 
       <main>
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <img src="/cgu.github.io/static/img_logo.png"></img>
+        <img src="/cgu.github.io /static/img_logo.png"></img>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
